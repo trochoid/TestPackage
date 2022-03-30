33d8d99305ee9071e7a07bdf180719f989b21f1a
trochoid <trochoid@users.noreply.github.com>
2022-03-30 03:51:17 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .target(name: "TestPackage", path: "TestPackage", exclude: [])
+        .target(name: "TestPackage", path: "TestPackage", exclude: ["Info.plist"])
     ]
 )
